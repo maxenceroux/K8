c44204ef69814e019a78c5a7a36cc94fed7e9cfe
maxenceroux <maxence.b.roux@gmail.com>
2021-03-08 20:10:36 +0100

    fixes common install

--- install_common.sh
+++ install_common.sh
@@ -10,7 +10,7 @@ echo "START - install common - "$IP
 echo "[1]: add host name for ip"
 host_exist=$(cat /etc/hosts | grep -i "$IP" | wc -l)
 if [ "$host_exist" == "0" ];then
-echo "$IP $HOSTNAME " >/etc/hosts
+echo "$IP $HOSTNAME " >>/etc/hosts
 fi
 
 echo "[2]: disable swap"
